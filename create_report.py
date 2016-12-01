dd37db91a5393d8685fa2e6be01a8cdc0309d791
Andrew Price <andrewprice@mail.weber.edu>
2016-11-30 21:36:05 -0700

    getInput() and dateFormat() updated, working

--- create_report.py
+++ create_report.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 import sys
+from datetime import datetime
 
 def getInput():
-    
+    while True:
+        try:
+            begTest = sys.argv[1]
+            endTest = sys.argv[2]
+            begTest = datetime.strptime(begTest, '%Y%m%d')
+            endTest = datetime.strptime(endTest,'%Y%m%d')
+            dateFormat(sys.argv[1],sys.argv[2])
+            return False
+        except ValueError:
+            print('Invalid date!')
+            break
 
+def dateFormat(begDate,endDate):
+    beg_date = datetime.strptime(begDate,'%Y%m%d').strftime('%Y-%m-%d 00:00')
+    end_date = datetime.strptime(sys.argv[2],'%Y%m%d').strftime('%Y-%m-%d 23:59')
+    print("Beginning date: {}".format(beg_date))
+    print("Ending date: {}".format(end_date))
+       
 def main():
-    """
-    Test Function
-    """
     getInput()
 
 if __name__ == '__main__':
