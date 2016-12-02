c962aad2ef707f6bae2f5e818b5a48d7b18d3c93
Karl <karlmarble@mail.weber.edu>
2016-12-02 05:52:27 +0000

    testing parameter pass

--- run_report.sh
+++ run_report.sh
@@ -13,7 +13,26 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+function reqHelp {
+	echo "Usage is run_report.sh [-f BegDate] [-t EndDate] [-e email] [-u FTP username] [-p FTP password]"
+	exit 1
+}
 
+while getopts ":f:t:e:u:p:"; do
+	case $opt in
+		f) beg_date=$OPTARG;;
+		t) end_date=$OPTARG;;
+		e) email=$OPTARG;;
+		u) user=$OPTARG;;
+		p) pass=$OPTARG;;
+		\?) reqHelp;;
+	esac
+done
+
+echo $beg_date
+echo $end_date
+
+#python3 ./create_report2.py $beg_date $end_date
 
 exit 0
 
